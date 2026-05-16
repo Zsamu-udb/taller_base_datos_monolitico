bc9ef24a1e225d6af2c576b5abfe173b75676a3b
Zsamu-udb <savalencia@uniboyaca.edu.co>
2026-05-16 10:40:05 -0500

    ahora shi

--- controllers/SprintController.php
+++ controllers/SprintController.php
@@ -16,15 +16,29 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $fecha_inicio = $_POST['fecha_inicio'] ?? '';
     $fecha_fin = $_POST['fecha_fin'] ?? '';
 
+    /* VALIDAR CAMPOS */
+
     if (
         !empty($nombre) &&
         !empty($fecha_inicio) &&
         !empty($fecha_fin)
     ) {
 
+        /* VALIDAR FECHAS */
+
+        if ($fecha_fin < $fecha_inicio) {
+
+            die("Error: la fecha final no puede ser menor a la fecha inicial.");
+
+        }
+
+        /* SETTERS */
+
         $sprint->setNombre($nombre);
         $sprint->setFechaInicio($fecha_inicio);
         $sprint->setFechaFin($fecha_fin);
+
+        /* CREAR SPRINT */
 
         $sprint->create();
     }
